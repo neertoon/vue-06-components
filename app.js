ed1beff619f03c4927516591c05745d1c47b8c67
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2025-02-03 08:09:21 +0100

    !! Wstęp do komponentów

--- app.js
+++ app.js
@@ -15,4 +15,28 @@ const app = Vue.createApp({
     }
 });
 
+app.component('user-contact', {
+    data() {
+        return  {
+            detailsAreVisible: false,
+            friend: { id: 'manuel', name: 'Gostek', phone: '234234 234', email: 'asdfa@ok.pl', details: false},
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    },
+    template: `
+    <li >
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails(friend.id)">{{ detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+    `
+});
+
 app.mount('#app')
